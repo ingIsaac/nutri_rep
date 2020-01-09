ba16cac06e377f1b932dbb371f00a0b50f4fe342
Isaac <ing_isaacsolis@hotmail.com>
2020-01-09 11:52:14 -0700

    pug added

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const socketIO = require('socket.io');
 const app = express();
 const port = 3000;
 const path = require('path');
-var _partida = require('./app/scripts/core_lib');
 
 app.set('port', port);
 app.use(express.static(path.join(__dirname, 'app')));
 app.set('views', path.join(__dirname + '/app','views'));
 app.set('view engine', 'pug');
-const server = app.listen(app.get('port'));
-var io = socketIO.listen(server);
 console.log('Servidor Iniciado... Puerto: ', port);
 //------------------------------------------------->
 
@@ -18,4 +14,7 @@ app.get("/",(req,res) => {
   res.render('index');
 });
 
+app.listen(port, () => {
+  console.log('Servidor Ejecutandose...');
+});
 //------------------------------------------------->
